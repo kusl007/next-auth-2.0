8683c238a9497f223862bba3d649a56f265afdd8
kusl007 <kushal.bansal.1001@gmail.com>
2025-05-27 21:39:56 +0545

    signup login complete

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
+
         mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
-
+        
+        
         connection.on('connected', () => {
             console.log('MongoDB connected successfully');
         })
